cde7d94b79c472b3d91278e752650292e81385ce
Ahmed-Abbasi-Official <farhad.khi17@gmail.com>
2025-08-23 02:48:17 +0500

    some changes

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,19 @@
+// import type { NextConfig } from "next";
+
+// const nextConfig: NextConfig = {
+//   /* config options here */
+// };
+
+// export default nextConfig;
+
+
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  eslint: {
+    ignoreDuringBuilds: true, // ✅ This disables ESLint errors during build
+  },
+  // You can add other config options here
 };
 
 export default nextConfig;
